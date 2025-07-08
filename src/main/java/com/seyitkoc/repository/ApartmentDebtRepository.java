f3b3a724d6016b1aea872f9cd33062f5614fae72
seyitalikoc <seyit.koc.006@gmail.com>
2025-07-08 22:44:41 +0300

    update project

--- src/main/java/com/seyitkoc/repository/ApartmentDebtRepository.java
+++
@@ -1,7 +0,0 @@
-package com.seyitkoc.repository;
-
-import com.seyitkoc.entity.debt.ApartmentDebts;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApartmentDebtRepository extends JpaRepository<ApartmentDebts, Long> {
-}
--- src/main/java/com/seyitkoc/service/IHashService.java
+++
@@ -1,8 +0,0 @@
-package com.seyitkoc.service;
-
-import java.time.LocalDateTime;
-
-public interface IHashService {
-    String calculateHash(Integer index, LocalDateTime timestamp, String voterName, Long voterApartmentId,  String voteOption, String previousHash);
-    String getGenesisHash();
-}
